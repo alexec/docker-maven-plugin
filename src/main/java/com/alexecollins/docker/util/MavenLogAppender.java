8797ba0b169a7a6055e2fbd8d2018b09c74826f6
Alex Collins <alex.e.c@gmail.com>
2014-04-27 11:42:38 +0100

    logging

--- src/main/java/com/alexecollins/docker/util/MavenLogAppender.java
+++ src/main/java/com/alexecollins/docker/util/MavenLogAppender.java
@@ -2,11 +2,22 @@ package com.alexecollins.docker.util;
 
 import ch.qos.logback.core.AppenderBase;
 import org.apache.maven.plugin.logging.Log;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class MavenLogAppender<E> extends AppenderBase<E> {
 
     @SuppressWarnings("FieldCanBeLocal")
     private static Log log;
+
+	static {
+		LogManager.getLogManager().reset();
+		SLF4JBridgeHandler.install();
+		Logger.getLogger("global").setLevel(Level.FINEST);
+	}
 
     public static void setLog(Log log) {
         MavenLogAppender.log = log;
