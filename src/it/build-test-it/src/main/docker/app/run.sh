bcabfb690c27a62d4fdd937afa0c57d008c86008
Alex Collins <alex.e.c@gmail.com>
2014-06-25 22:02:56 +0100

    support removal or intermediate containers

--- src/it/build-test-it/src/main/docker/app/run.sh
+++ src/it/build-test-it/src/main/docker/app/run.sh
@@ -3,5 +3,5 @@ set -eux
 
 sed -i "s/localhost:3306/$EXAMPLE_MYSQL_PORT_3306_TCP_ADDR:3306/" hello-world.yml
 
-java -jar example-1.0-SNAPSHOT.jar db migrate hello-world.yml
-java -jar example-1.0-SNAPSHOT.jar server hello-world.yml
+java -jar example-${project.version}.jar db migrate hello-world.yml
+java -jar example-${project.version}.jar server hello-world.yml
