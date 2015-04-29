e376ce7aa6bcaf98ad10a570c7a82042c0bf40c5
alex.collins <alex.collins@gamesys.co.uk>
2015-04-29 17:40:01 +0100

    fixed it

--- src/it/port-setup-it/verify.groovy
+++ src/it/port-setup-it/verify.groovy
@@ -1,4 +1,4 @@
-def actualPos = "docker ps".execute().text
+def actualPos = new File(basedir, "docker.ps").text
 def expectedSubstring = "0.0.0.0:8888->3306/tcp"
 
 assert actualPos.contains(expectedSubstring), "\"" + actualPos + "\" contains \"" + expectedSubstring + "\""
