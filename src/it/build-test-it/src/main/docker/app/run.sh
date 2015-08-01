3d1dc1b6c38cf6b266c83acbb648ea2f081420e7
Alex Collins <alex.e.c@gmail.com>
2015-08-01 19:05:06 +0100

    update port = 3306

--- src/it/build-test-it/src/main/docker/app/run.sh
+++ src/it/build-test-it/src/main/docker/app/run.sh
@@ -4,8 +4,8 @@ set -eux
 env
 
 # by default we use the long name of the project, but we can use an alias instead
-# sed -i "s/localhost:3306/$EXAMPLE_PROJECT_MYSQL_PORT_3306_TCP_ADDR:3306/" hello-world.yml
-sed -i "s/localhost:3306/$DB_PORT_3306_TCP_ADDR:3306/" hello-world.yml
+# sed -i "s/localhost:3306/$EXAMPLE_PROJECT_MYSQL_PORT_3306_TCP_ADDR:${mysql.port}/" hello-world.yml
+sed -i "s/localhost:3306/$DB_PORT_3306_TCP_ADDR:${mysql.port}/" hello-world.yml
 
 java -jar ${project.build.finalName}.jar db migrate hello-world.yml
 java -jar ${project.build.finalName}.jar server hello-world.yml
